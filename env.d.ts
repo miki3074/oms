ab751dc24f84280272c2d0cdb0e2ef5432cd8568
miki3074 <miki23074@gmail.com>
2025-12-28 03:24:47 +0300

    Add pp

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// 2. Исправляет ошибку с v-video-embed
+declare module 'v-video-embed';
